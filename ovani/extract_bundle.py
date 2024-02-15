b6f0f755dd0589866f306fe8e2f5572f75c7fa3a
Enrique Pelaez <enrique.pel@gmail.com>
2024-02-15 16:37:48 +0700

    Made some improvements to make files more accessible

--- ovani/extract_bundle.py
+++ ovani/extract_bundle.py
@@ -4,23 +4,35 @@
 # python extract_bundle.py "C:/Ovani Sound"
 # python extract_bundle.py "C:/Ovani Sound" main_only
 
-import glob, zipfile, sys, os, io
+import glob, zipfile, sys, os, io, shutil
+
 
 def extract_wavs(zip_file_path, extract_to_path):
     with zipfile.ZipFile(zip_file_path, "r") as zip_file:
         file_list = zip_file.infolist()
         for file in file_list:
-            file_name = file.filename
-            print(file_name)
-            if file_name.find(".wav") > -1 and (
+            file_name_lc = file.filename.lower()
+            print(file_name_lc)
+            if ".wav" in file_name_lc and (
                 not main_only
-                or file_name.find("Main") > -1
-                or file_name.find("Sound FX") > -1
-                or file_name.find("SFX") > -1
+                or "main" in file_name_lc
+                or "sound fx" in file_name_lc
+                or "sfx" in file_name_lc
             ):
-                print("will extract " + file_name)
+                print("will extract " + file.filename)
                 zip_file.extract(file, extract_to_path)
-            if file_name.find(".zip") > -1:
+                if main_only and "main" in file_name_lc:
+                    file_path = os.path.join(extract_to_path, file.filename)
+                    file_name = os.path.basename(file_path)
+                    parent_dir = os.path.dirname(file_path)
+                    target_dir = os.path.dirname(parent_dir)
+                    target_path = os.path.join(target_dir, file_name)
+                    # removing "main" from the name and deleting the extra directory
+                    target_path = target_path.replace(" Main", "").replace(" main", "")
+                    shutil.move(file_path, target_path)
+                    os.rmdir(parent_dir)
+
+            if ".zip" in file_name_lc:
                 with zip_file.open(file) as inner_zip_file:
                     inner_zip_data = io.BytesIO(inner_zip_file.read())
                     extract_wavs(inner_zip_data, extract_to_path)
@@ -41,7 +53,7 @@ if not os.path.exists(folder_path):
     print("Folder " + folder_path + " does not exist")
     exit(1)
 
-extract_to_path = folder_path + "/output"
+extract_to_path = os.path.join(folder_path, "output")
 os.makedirs(extract_to_path, exist_ok=True)
 
 zip_files_paths = glob.glob(f"{folder_path}/*.zip")
@@ -49,4 +61,3 @@ zip_files_paths = glob.glob(f"{folder_path}/*.zip")
 
 for zip_file_path in zip_files_paths:
     extract_wavs(zip_file_path, extract_to_path)
-
